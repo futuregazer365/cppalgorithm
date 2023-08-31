0eb6dcb3069150c51ce46363a4ea08d19ef58a9d
哲贤 苏 <2030150139@qq.com>
2023-08-31 16:57:52 +0800

    20230831

--- 组合总和2/组合总和2.cpp
+++ 组合总和2/组合总和2.cpp
@@ -1,5 +1,5 @@
 ﻿// 组合总和2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+// leetcode40
 #include <vector>
 #include <stack>
 #include <algorithm>
@@ -9,50 +9,44 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end()); // 排序候选集
-        vector<int> current; // 当前正在形成的组合
-        backtrack(candidates, target, 0, current);
-
-        // 对结果进行去重操作
-        set<vector<int>> uniqueResults(result.begin(), result.end());
-        result.assign(uniqueResults.begin(), uniqueResults.end());
-
+        path.clear();
+        result.clear();
+        sort(candidates.begin(), candidates.end());//注意candidates的排序方法
+        vector<bool> used(candidates.size(), false);//注意used的初始化方式
+        backtracking(target, 0, 0, candidates, used);
         return result;
     }
-
-private:
-    vector<vector<int>> result;
-
-    void backtrack(vector<int>& candidates, int target, int startIndex, vector<int>& current) {
-        if (target == 0) {
-            result.push_back(current); // 找到一个符合条件的组合
+    void backtracking(int targetSum, int sum, int startIndex, const vector<int>& candidates, vector<bool>& uesd) {
+        //递归中断
+        if (sum > targetSum)return;
+        if (sum == targetSum) {
+            result.push_back(path);
             return;
         }
-
         for (int i = startIndex; i < candidates.size(); i++) {
-            if (i > startIndex && candidates[i] == candidates[i - 1]) {
-                continue; // 避免重复的组合
+            //去重
+            if (i > 0 && candidates[i] == candidates[i - 1] && uesd[i - 1] == false) {
+                continue;
             }
-
-            if (target - candidates[i] < 0) {
-                break; // 剪枝
-            }
-
-            current.push_back(candidates[i]); // 尝试将当前数字加入组合
-            backtrack(candidates, target - candidates[i], i + 1, current); // 递归调用
-            current.pop_back(); // 回溯，撤销选择
+            path.push_back(candidates[i]);
+            sum += candidates[i];
+            uesd[i] = true;
+            backtracking(targetSum, sum, i + 1, candidates, uesd);
+            path.pop_back();
+            sum -= candidates[i];
+            uesd[i] = false;
         }
     }
+private:
+    vector<int> path;
+    vector<vector<int>> result;
 };
 int main()
 {
-    set<vector<int>> temp;
-    vector<int> vec1 = { 1,7 };
-    vector<int> vec3 = { 1,7 };
-    vector<int> vec2 = { 7,1 };
-    temp.insert(vec1);
-    temp.insert(vec2);
-    temp.insert(vec3);
+    vector<int> vec1 = { 10,1,2,7,6,1,5 };
+    vector<int> vec2 = { 1,1,2};
+    Solution S;
+    vector<vector<int>> result = S.combinationSum2(vec1, 8);
     std::cout << "Hello World!\n";
 }
 
